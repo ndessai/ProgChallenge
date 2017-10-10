96ee749741300ba34367ffac15fe1b987881eafb
Nav Dessai <ndessai@gmail.com>
2017-10-10 00:30:32 -0400

    adding findpairs controller

---
+++ Server/Controllers/FindPairsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Server.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/FindPairs")]
+    public class FindPairsController : Controller
+    {
+        // GET: api/FindPairs
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // POST: api/FindPairs
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+        
+    }
+}
